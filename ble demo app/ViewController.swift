7ced40ee1600a3ab2e750eba6e7ef197a95b88a6
Ayaz Alavi <ayaz.alavi@gmail.com>
2020-09-21 11:54:05 +0500

    Initial Commit

---
+++ ble demo app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ble demo app
+//
+//  Created by Miranz  Technologies on 9/21/20.
+//  Copyright © 2020 Ayaz Alavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ble demo appTests/ble_demo_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  ble_demo_appTests.swift
+//  ble demo appTests
+//
+//  Created by Miranz  Technologies on 9/21/20.
+//  Copyright © 2020 Ayaz Alavi. All rights reserved.
+//
+
+import XCTest
+@testable import ble_demo_app
+
+class ble_demo_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
