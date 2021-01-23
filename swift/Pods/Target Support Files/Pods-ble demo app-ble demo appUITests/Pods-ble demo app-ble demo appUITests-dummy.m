7abc056d42ce6859a23030c0f5f49190fee9b293
Ayaz Alavi <ayaz.alavi@gmail.com>
2021-01-23 23:48:18 +0500

    removing pods folder

--- swift/Pods/Target Support Files/Pods-ble demo app-ble demo appUITests/Pods-ble demo app-ble demo appUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ble_demo_app_ble_demo_appUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ble_demo_app_ble_demo_appUITests
-@end
--- swift/Pods/Target Support Files/Pods-ble demo app/Pods-ble demo app-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ble_demo_app : NSObject
-@end
-@implementation PodsDummy_Pods_ble_demo_app
-@end
